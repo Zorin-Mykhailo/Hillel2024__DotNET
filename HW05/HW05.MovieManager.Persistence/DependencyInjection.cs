b44a630638b86472cb85390287cdcf8e1f143bc0
Zorin-Mykhailo <zorin.mikhail.84284@gmail.com>
2024-04-11 13:34:55 +0300

    WIP

--- HW05/HW05.MovieManager.Persistence/DependencyInjection.cs
+++ HW05/HW05.MovieManager.Persistence/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HW05.MovieManager.Application.Interfaces;
+using HW05.MovieManager.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +9,9 @@ public static class DependencyInjection
 {
     public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+                configuration.GetConnectionString("AppDb"), b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
 
+        services.AddScoped<IAppDbContext>(provider => provider.GetService<AppDbContext>()!);
     }
 }
